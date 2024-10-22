a32512dfb8860a4d6cb90d0d796095bec6c60fb3
Alvaro Belmonte Baeza <alvaro.bb.7@gmail.com>
2024-10-22 11:25:08 +0900

    modify spherical commands to interpolate to target pose

--- source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/locomanipulation/reach/mdp/commands/pose_spherical_command.py
+++ source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/locomanipulation/reach/mdp/commands/pose_spherical_command.py
@@ -14,7 +14,8 @@ from typing import TYPE_CHECKING
 from omni.isaac.lab.assets import Articulation
 from omni.isaac.lab.managers import CommandTerm
 from omni.isaac.lab.markers import VisualizationMarkers
-from omni.isaac.lab.utils.math import combine_frame_transforms, compute_pose_error, quat_from_euler_xyz, quat_unique, transform_points
+from omni.isaac.lab.utils.math import combine_frame_transforms, subtract_frame_transforms, compute_pose_error, quat_from_euler_xyz, quat_unique, quat_inv
+import pytorch3d.transforms as pt3d
 
 if TYPE_CHECKING:
     from omni.isaac.lab.envs import ManagerBasedEnv
@@ -60,10 +61,15 @@ class UniformSphericalPoseCommand(CommandTerm):
 
         # create buffers
         # -- commands: (x, y, z, qw, qx, qy, qz) in root frame
-        self.pose_command_spherical = torch.zeros(self.num_envs, 3, device=self.device)
-        self.pose_command_cartesian = torch.zeros(self.num_envs, 7, device=self.device)
-        self.pose_command_cartesian[:, 3] = 1.0
-        self.pose_command_w = torch.zeros_like(self.pose_command_cartesian)
+        self.pose_command_spherical_b = torch.zeros(self.num_envs, 7, device=self.device)
+        self.pose_command_spherical_b[:, 3] = 1.0
+        self.pose_command_cartesian_b = torch.zeros(self.num_envs, 7, device=self.device)
+        self.pose_command_cartesian_b[:, 3] = 1.0
+        self.pose_command_w = torch.zeros_like(self.pose_command_cartesian_b)
+        self.curr_ee_target_b = torch.zeros_like(self.pose_command_spherical_b)
+        # -- initial end-effector pose
+        self.init_ee_pose_w = torch.zeros(self.num_envs, 7, device=self.device)
+        self.init_ee_pose_b = torch.zeros_like(self.init_ee_pose_w)
         # -- metrics
         self.metrics["position_error"] = torch.zeros(self.num_envs, device=self.device)
         self.metrics["orientation_error"] = torch.zeros(self.num_envs, device=self.device)
@@ -80,24 +86,45 @@ class UniformSphericalPoseCommand(CommandTerm):
 
     @property
     def command(self) -> torch.Tensor:
-        """The desired pose command. Shape is (num_envs, 9).
-
-        It corresponds to the 3D positions of 3 vertices of a cube of side cube_size centered at the body's origin. so the keypoints are:
-        [x1,y1,z1, x2,y2,z2, x3,y3,z3]
         """
-        return self.pose_command_w
+        Returns the spherical pose command.
+        This method retrieves the spherical pose command tensor, which is stored in the 
+        `pose_command_spherical_b` attribute.
+        Returns:
+            torch.Tensor: The spherical pose command tensor.
+        """
+ 
+        return self.curr_ee_target_b
     
     """
     Implementation specific functions.
     """
 
+    def spherical_to_cartesian(self, spherical_pose: torch.Tensor) -> torch.Tensor:
+        cartesian_pose = torch.zeros_like(spherical_pose)
+        cartesian_pose[:, 0] = spherical_pose[:, 0] * torch.sin(spherical_pose[:, 1]) * torch.cos(spherical_pose[:, 2])
+        cartesian_pose[:, 1] = spherical_pose[:, 0] * torch.sin(spherical_pose[:, 1]) * torch.sin(spherical_pose[:, 2])
+        cartesian_pose[:, 2] = spherical_pose[:, 0] * torch.cos(spherical_pose[:, 1])
+        cartesian_pose[:, 3:] = spherical_pose[:, 3:]
+        return cartesian_pose
+    
+    def cartesian_to_spherical(self, cartesian_pose: torch.Tensor) -> torch.Tensor:
+        spherical_pose = torch.zeros_like(cartesian_pose)
+        r = torch.norm(cartesian_pose[:, :3], dim=-1)
+        spherical_pose[:, 0] = r
+        spherical_pose[:, 1] = torch.acos(cartesian_pose[:, 2] / r)
+        spherical_pose[:, 2] = torch.atan2(cartesian_pose[:, 1], cartesian_pose[:, 0])
+        spherical_pose[:, 3:] = cartesian_pose[:, 3:]
+        return spherical_pose
+    
     def _update_metrics(self):
         # transform command from base frame to simulation world frame
+        curr_ee_target_cart_b = self.spherical_to_cartesian(self.curr_ee_target_b)
         self.pose_command_w[:, :3], self.pose_command_w[:, 3:] = combine_frame_transforms(
             self.robot.data.root_pos_w,
             self.robot.data.root_quat_w,
-            self.pose_command_cartesian[:, :3],
-            self.pose_command_cartesian[:, 3:],
+            curr_ee_target_cart_b[:, :3],
+            curr_ee_target_cart_b[:, 3:],
         )
         # compute the error
         pos_error, rot_error = compute_pose_error(
@@ -113,21 +140,36 @@ class UniformSphericalPoseCommand(CommandTerm):
         # sample new pose targets
         # -- spherical coordinates
         r = torch.empty(len(env_ids), device=self.device)
-        self.pose_command_spherical[env_ids, 0] = r.uniform_(*self.cfg.ranges.radius)
-        self.pose_command_spherical[env_ids, 1] = r.uniform_(*self.cfg.ranges.pitch)
-        self.pose_command_spherical[env_ids, 2] = r.uniform_(*self.cfg.ranges.yaw)
+        self.pose_command_spherical_b[env_ids, 0] = r.uniform_(*self.cfg.sphere_ranges.radius)
+        self.pose_command_spherical_b[env_ids, 1] = r.uniform_(*self.cfg.sphere_ranges.polar)
+        self.pose_command_spherical_b[env_ids, 2] = r.uniform_(*self.cfg.sphere_ranges.azimuth)
         # -- orientation
-        euler_angles = torch.zeros_like(self.pose_command_cartesian[env_ids, :3])
-        euler_angles[:, 0].uniform_(*self.cfg.ranges.roll)
-        euler_angles[:, 1].uniform_(*self.cfg.ranges.pitch)
-        euler_angles[:, 2].uniform_(*self.cfg.ranges.yaw)
+        euler_angles = torch.zeros_like(self.pose_command_spherical_b[env_ids, :3])
+        euler_angles[:, 0].uniform_(*self.cfg.sphere_ranges.roll)
+        euler_angles[:, 1].uniform_(*self.cfg.sphere_ranges.pitch)
+        euler_angles[:, 2].uniform_(*self.cfg.sphere_ranges.yaw)
         quat = quat_from_euler_xyz(euler_angles[:, 0], euler_angles[:, 1], euler_angles[:, 2])
-        # make sure the quaternion has real part as positive
-        self.pose_command_cartesian[env_ids, 3:] = quat_unique(quat) if self.cfg.make_quat_unique else quat
-        # convert the pose to keypoints
+        quat = quat_unique(quat) if self.cfg.make_quat_unique else quat
+        self.pose_command_spherical_b[env_ids, 3:] = quat
+        # Convert the sampled spherical coord to cartesian
+        self.pose_command_cartesian_b[env_ids] = self.spherical_to_cartesian(self.pose_command_spherical_b[env_ids])
+        self.curr_ee_target_b[env_ids, 3:] = self.pose_command_spherical_b[env_ids, 3:]
+
+        self.init_ee_pose_w[env_ids, :] = self.robot.data.body_state_w[env_ids, self.body_idx, :7].clone()
+        # Convert the initial end-effector pose from world frame to base frame
+        self.init_ee_pose_b[env_ids, :3], self.init_ee_pose_b[env_ids, 3:] = subtract_frame_transforms(
+            self.robot.data.root_pos_w[env_ids],
+            self.robot.data.root_quat_w[env_ids],
+            self.init_ee_pose_w[env_ids, :3],
+            self.init_ee_pose_w[env_ids, 3:],
+        )
+        self.init_ee_pose_spherical_b = self.cartesian_to_spherical(self.init_ee_pose_b)
 
     def _update_command(self):
-        pass
+        # Get current time for the command (clipped to 0,1)
+        t = torch.clip((self.cfg.resampling_time_range[1] - self.time_left) / self.cfg.resampling_time_range[1], 0, 1).reshape(-1, 1)
+        # Interpolate between the current and the target command
+        self.curr_ee_target_b[:, :3] = torch.lerp(self.init_ee_pose_spherical_b[:, :3], self.pose_command_spherical_b[:, :3], t)
 
     def _set_debug_vis_impl(self, debug_vis: bool):
         # create markers if necessary for the first tome
