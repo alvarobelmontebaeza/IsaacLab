af8844f8a02987462b001c7088e02b7a0b17ec48
Alvaro Belmonte Baeza <alvaro.bb.7@gmail.com>
2024-11-15 10:53:33 +0900

    limit output torque as in real K1

--- source/extensions/omni.isaac.lab_assets/omni/isaac/lab_assets/sagittarius.py
+++ source/extensions/omni.isaac.lab_assets/omni/isaac/lab_assets/sagittarius.py
@@ -13,7 +13,7 @@ Reference: https://github.com/ros-industrial/universal_robot
 """
 
 import omni.isaac.lab.sim as sim_utils
-from omni.isaac.lab.actuators import ImplicitActuatorCfg
+from omni.isaac.lab.actuators import ImplicitActuatorCfg, IdealPDActuatorCfg
 from omni.isaac.lab.assets.articulation import ArticulationCfg
 from omni.isaac.lab.utils.assets import ISAACLAB_ASSETS_DIR
 ##
@@ -28,7 +28,7 @@ K1_CFG = ArticulationCfg(
         rigid_props=sim_utils.RigidBodyPropertiesCfg(
             disable_gravity=False,
             max_depenetration_velocity=5.0,
-        ),
+        ),  
         articulation_props=sim_utils.ArticulationRootPropertiesCfg(
             enabled_self_collisions=True, solver_position_iteration_count=8, solver_velocity_iteration_count=0
         ),
@@ -47,14 +47,22 @@ K1_CFG = ArticulationCfg(
     actuators={
         "arm_low_torque": ImplicitActuatorCfg(
             joint_names_expr=["K1_shoulder_pan_joint", "K1_wrist_1_joint", "K1_wrist_2_joint", "K1_wrist_3_joint"],
-            effort_limit=4.41,
+            effort_limit={
+                "K1_shoulder_pan_joint": 0.3 * 4.41,
+                "K1_wrist_1_joint": 0.2 * 4.41,
+                "K1_wrist_2_joint": 0.3 * 4.41,
+                "K1_wrist_3_joint": 0.2 * 4.41,
+            },
             velocity_limit=3.14,
             stiffness=5.0,
             damping=1.0,
         ),
         "arm_high_torque": ImplicitActuatorCfg(
             joint_names_expr=["K1_shoulder_lift_joint", "K1_elbow_joint"],
-            effort_limit=8.33,
+            effort_limit={
+                "K1_shoulder_lift_joint": 0.3 * 8.33,
+                "K1_elbow_joint": 0.3 * 8.33,
+            },
             velocity_limit=3.14,
             stiffness=5.0,
             damping=1.0,
