e01c5068f61bc4d5007e2f10937f30b60be82af3
TMats <t.matsu.changmin@gmail.com>
2024-11-21 18:00:20 +0900

    fix error in robot cfg being loaded

--- source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/locomanipulation/reach/config/k1go1/rough_env_cfg.py
+++ source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/locomanipulation/reach/config/k1go1/rough_env_cfg.py
@@ -19,7 +19,7 @@ class K1Go1ReachRoughEnvCfg(LocomanipulationReachRoughEnvCfg):
         # post init of parent
         super().__post_init__()
 
-        self.scene.robot = UNITREE_GO1_CFG.replace(prim_path="{ENV_REGEX_NS}/Robot")
+        self.scene.robot = K1GO1_CFG.replace(prim_path="{ENV_REGEX_NS}/Robot")
         self.scene.height_scanner.prim_path = "{ENV_REGEX_NS}/Robot/trunk"
         # scale down the terrains because the robot is small
         self.scene.terrain.terrain_generator.sub_terrains["boxes"].grid_height_range = (0.025, 0.1)
