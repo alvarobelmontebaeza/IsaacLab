24616d72fd56839cba396e3be60674c416c8153d
Alvaro Belmonte Baeza <alvaro.bb.7@gmail.com>
2025-02-14 10:08:49 +0100

    Add Constrained env type to the valid envs for training script

--- source/standalone/workflows/rsl_rl/train.py
+++ source/standalone/workflows/rsl_rl/train.py
@@ -56,6 +56,7 @@ from omni.isaac.lab.envs import (
     DirectMARLEnvCfg,
     DirectRLEnvCfg,
     ManagerBasedRLEnvCfg,
+    ConstrainedManagerBasedRLEnvCfg,
     multi_agent_to_single_agent,
 )
 from omni.isaac.lab.utils.dict import print_dict
@@ -73,7 +74,7 @@ torch.backends.cudnn.benchmark = False
 
 
 @hydra_task_config(args_cli.task, "rsl_rl_cfg_entry_point")
-def main(env_cfg: ManagerBasedRLEnvCfg | DirectRLEnvCfg | DirectMARLEnvCfg, agent_cfg: RslRlOnPolicyRunnerCfg):
+def main(env_cfg: ManagerBasedRLEnvCfg | ConstrainedManagerBasedRLEnvCfg | DirectRLEnvCfg | DirectMARLEnvCfg, agent_cfg: RslRlOnPolicyRunnerCfg):
     """Train with RSL-RL agent."""
     # override configurations with non-hydra CLI arguments
     agent_cfg = cli_args.update_rsl_rl_cfg(agent_cfg, args_cli)
