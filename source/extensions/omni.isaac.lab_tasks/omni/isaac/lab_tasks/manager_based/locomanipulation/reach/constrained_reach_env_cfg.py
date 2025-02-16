bbec9a41ac53d6b8115c2fbaba956aa4d7f3ddbb
Alvaro Belmonte Baeza <alvaro.bb.7@gmail.com>
2025-02-16 20:02:58 +0100

    activate contact termination again

--- source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/locomanipulation/reach/constrained_reach_env_cfg.py
+++ source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/locomanipulation/reach/constrained_reach_env_cfg.py
@@ -326,12 +326,10 @@ class TerminationsCfg:
     """Termination terms for the MDP."""
 
     time_out = DoneTerm(func=mdp.time_out, time_out=True)
-    '''    
     base_contact = DoneTerm(
         func=mdp.illegal_contact,
         params={"sensor_cfg": SceneEntityCfg("contact_forces", body_names=[".*base", ".*hip"]), "threshold": 1.0},
     )
-    '''
     
     # bad_orientation = DoneTerm(func=mdp.bad_orientation, params={"limit_angle": 0.5, "asset_cfg": SceneEntityCfg("robot", body_names=["base"])})
 
