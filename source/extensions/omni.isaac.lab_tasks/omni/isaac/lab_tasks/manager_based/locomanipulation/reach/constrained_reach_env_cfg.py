1f8ca055483b00b12ac4d27c4701579c5d0345a5
Alvaro Belmonte Baeza <alvaro.bb.7@gmail.com>
2025-03-09 18:09:08 +0100

    include upside down constraint

--- source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/locomanipulation/reach/constrained_reach_env_cfg.py
+++ source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/locomanipulation/reach/constrained_reach_env_cfg.py
@@ -344,13 +344,18 @@ class ConstraintsCfg:
                                        params={"limit": 1000.0, "sensor_cfg": SceneEntityCfg("contact_forces", body_names=[".*foot"])})
     cstr_min_base_height = CstrTerm(init_max_p=1.0, final_max_p=1.0, func=mdp.cstr_min_base_height,
                                     params={"min_height": 0.15, "asset_cfg": SceneEntityCfg("robot", body_names=[".*base"])})
+    cstr_upside_down = CstrTerm(init_max_p=1.0, final_max_p=1.0, func=mdp.cstr_upsidedown,
+                                params={"asset_cfg": SceneEntityCfg("robot", body_names=[".*base"])})
+    
     # -- STYLE CONSTRAINTS
+    cstr_body_velocity = CstrTerm(init_max_p=0.05, final_max_p=0.25, func=mdp.cstr_max_base_velocity,
+                                 params={"limit": 0.2, "asset_cfg": SceneEntityCfg("robot", body_names=[".*base"])})
     cstr_joint_deviation = CstrTerm(init_max_p=0.05, final_max_p=0.25, func=mdp.cstr_joint_deviation,
                                     params={"limit": 0.2, "asset_cfg": SceneEntityCfg("robot", joint_names=[".*hip_joint"])})
-    cstr_base_orientation = CstrTerm(init_max_p=0.05, final_max_p=0.25, func=mdp.cstr_flat_orientation,
-                                     params={"limit": 0.2, "asset_cfg": SceneEntityCfg("robot", body_names=[".*base"])})
-    cstr_body_orientation_pitch = CstrTerm(init_max_p=0.05, final_max_p=0.25, func=mdp.cstr_body_orientation_axis,
-                                          params={"axis": "y", "limit": 0.1, "asset_cfg": SceneEntityCfg("robot", body_names=[".*base"])})
+    cstr_base_orientation = CstrTerm(init_max_p=0.05, final_max_p=0.9, func=mdp.cstr_flat_orientation,
+                                     params={"limit": 0.3, "asset_cfg": SceneEntityCfg("robot", body_names=[".*base"])})
+    cstr_body_orientation_pitch = CstrTerm(init_max_p=0.05, final_max_p=0.9, func=mdp.cstr_body_orientation_axis,
+                                          params={"axis": "y", "limit": 0.2, "asset_cfg": SceneEntityCfg("robot", body_names=[".*base"])})
     # cstr_no_move = CstrTerm(init_max_p=0.05, final_max_p=0.25, func=mdp.cstr_no_movement, params={"limit": 0.3, "command_name": "ee_pose", "asset_cfg": SceneEntityCfg("robot", joint_names=[".*hip_joint", ".*thigh_joint", ".*calf_joint"])})
     cstr_foot_stumble = CstrTerm(init_max_p=0.05, final_max_p=0.25, func=mdp.cstr_foot_stumble,
                                  params={"coeff": 2.0, "sensor_cfg": SceneEntityCfg("contact_forces", body_names=[".*foot"])})
